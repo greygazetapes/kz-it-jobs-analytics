aa06a224d1a779e5b00b6242890e550f9b97a5f1
greygazetapes <neilperry398@gmail.com>
2026-09-11 21:33:59 +0500

    feat: initial commit with README and raw fetcher

---
+++ README.md
@@ -0,0 +1,6 @@
+# Analysis of IT jobs in HH.kz
+
+## Project architecture
+1. Backend-part / ETL-pipeline
+
+2. Frontend-part
---
+++ fetch_raw.py
@@ -0,0 +1,10 @@
+import requests
+
+
+BASE_URL = "https://api.hh.ru/"
+
+DEFAULT_HEADERS = {
+    'User-Agent': 'KzITJobAnalytics/1.0 neilperry398@gmail.com',
+}
+
+
